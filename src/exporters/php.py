343fe8ad5204591bbef0025f192fbd3fa13716c2
Paulo Freitas <me@paulofreitas.me>
2016-07-16 09:44:21 -0300

    Improved PHP exporter module implementation.
    - Added support for Python 3.

--- src/exporters/php.py
+++ src/exporters/php.py
@@ -24,6 +24,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
+from __future__ import absolute_import
 
 # -- Imports ------------------------------------------------------------------
 
@@ -33,7 +34,7 @@ import phpserialize
 
 # Package modules
 
-from base import BaseExporter
+from .base import BaseExporter
 
 # -- Implementation -----------------------------------------------------------
 
