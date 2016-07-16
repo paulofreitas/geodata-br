51860548f780bc1fa697fd166fd33b79bc825bf3
Paulo Freitas <me@paulofreitas.me>
2016-07-16 09:52:38 -0300

    Improved SQLite exporter module implementation.
    - Added a new Sqlite3Exporter.binary_format class property to instruct export file handling;
    - Improved Sqlite3Exporter.__str__() method to auto-close file handling;
    - Improved Sqlite3Exporter.__str__() method unicode handling.

--- src/exporters/sqlite.py
+++ src/exporters/sqlite.py
@@ -24,10 +24,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
+from __future__ import absolute_import, unicode_literals
 
 # -- Imports ------------------------------------------------------------------
-
-from __future__ import absolute_import
 
 # Built-in modules
 
@@ -46,16 +45,18 @@ class Sqlite3Exporter(BaseExporter):
     '''SQLite3 exporter class.'''
     format = 'SQLite3'
     extension = '.sqlite3'
+    binary_format = True
 
     def __str__(self):
-        sql_str = str(SqlExporter(self._data, self._minified, dialect='sqlite'))
+        sql_str = SqlExporter(self._data, self._minified, dialect='sqlite').data
 
-        with tempfile.NamedTemporaryFile() as sqlite_file:
-            with sqlite3.connect(sqlite_file.name) as sqlite_con:
+        with tempfile.NamedTemporaryFile() as _sqlite_file:
+            with sqlite3.connect(_sqlite_file.name) as sqlite_con:
                 sqlite_cursor = sqlite_con.cursor()
                 sqlite_cursor.execute('PRAGMA page_size = 1024;')
                 sqlite_cursor.executescript('BEGIN; {} COMMIT'.format(sql_str))
 
-            sqlite_data = open(sqlite_file.name, 'rb').read()
+            with open(_sqlite_file.name, 'rb') as sqlite_file:
+                sqlite_data = sqlite_file.read()
 
         return sqlite_data
