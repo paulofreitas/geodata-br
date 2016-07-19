3bad194e7dfdc1889ec05400d9903d5b91585b08
Paulo Freitas <me@paulofreitas.me>
2016-07-19 03:59:23 -0300

    Tweaked parsers package metadata
    - Fixed parsers modules importing.

--- src/dtb/parsers/__init__.py
+++ src/dtb/parsers/__init__.py
@@ -24,7 +24,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
-
 # -- Metadata -----------------------------------------------------------------
 
 __all__ = ['xls']
@@ -33,7 +32,7 @@ __all__ = ['xls']
 
 # Parsers modules
 
-import xls
+from . import xls
 
 # -- Constants ----------------------------------------------------------------
 
