f2934274dd6c621f342c01365b7c46fca2aa7ebd
Paulo Freitas <me@paulofreitas.me>
2016-07-24 17:09:49 -0300

    Cleaned up core helpers module
    - Moved CliParser class into a new cli.base module;
    - Cleaned up unused imports.

--- src/dtb/core/helpers.py
+++ src/dtb/core/helpers.py
@@ -28,10 +28,7 @@ THE SOFTWARE.
 
 # Built-in dependencies
 
-import argparse
 import json
-import logging
-import sys
 
 from collections import defaultdict
 from os import listdir
@@ -113,66 +110,6 @@ class File(object):
     @property
     def size(self):
         return filesize(self._filename)
-
-
-class CliParser(object):
-    def __init__(self, **metadata):
-        self._metadata = metadata
-        self._parser = argparse.ArgumentParser(**metadata)
-        self._parser.conflict_handler = 'resolve'
-        self._parser.formatter_class = argparse.RawTextHelpFormatter
-        self._arguments = {}
-
-        self.setDefaults()
-        self.setLogging()
-
-    def addArgumentGroup(self, key, name):
-        self._arguments[key] = self._parser.add_argument_group(name)
-
-    def addArgument(self, group, *args, **kwargs):
-        self._arguments[group].add_argument(*args, **kwargs)
-
-    def setDefaults(self):
-        self.addArgumentGroup('global', 'Global options')
-        self.addArgument('global',
-                         '-h', '--help',
-                         action='help',
-                         help='Display this information'
-        )
-        self.addArgument('global',
-                         '-v', '--version',
-                         action='version',
-                         version='%(prog)s ' + self._metadata.get('__version__', ''),
-                         help='Show version information and exit'
-        )
-        self.addArgument('global',
-                         '-V', '--verbose',
-                         action='store_true',
-                         help='Display informational messages and warnings'
-        )
-
-    def setLogging(self):
-        # Setup stream handler
-        stream_formatter = logging.Formatter('%(message)s')
-        stream_handler = logging.StreamHandler(sys.stdout)
-        stream_handler.setFormatter(stream_formatter)
-
-        self._logger = logging.getLogger('dtb')
-        self._logger.addHandler(stream_handler)
-        self._logger.setLevel(logging.INFO)
-
-    def configure(self):
-        pass
-
-    def parse(self):
-        self.configure()
-
-        args = self._parser.parse_args()
-
-        if args.verbose:
-            self._logger.setLevel(logging.DEBUG)
-
-        return args
 
 
 # Markdown generator
