146f5b4c9066b255a9b34170e3d120c3aedb922b
Paulo Freitas <me@paulofreitas.me>
2016-07-23 22:00:46 -0300

    Added a new firebird format module

---
+++ src/dtb/formats/firebird.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Brazilian territorial distribution data exporter
+
+The MIT License (MIT)
+
+Copyright (c) 2013-2016 Paulo Freitas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+from __future__ import absolute_import
+
+# Imports
+
+# Package dependencies
+
+from .base import Format
+
+# Classes
+
+
+class FirebirdFormat(Format):
+    '''The file format class for Firebird Embedded file format.'''
+
+    @property
+    def name(self):
+        '''The file format name.'''
+        return 'firebird'
+
+    @property
+    def friendlyName(self):
+        '''The file format friendly name.'''
+        return 'Firebird Embedded'
+
+    @property
+    def extension(self):
+        '''The file format extension.'''
+        return '.fdb'
+
+    @property
+    def type(self):
+        '''The file format type.'''
+        return 'Database'
+
+    @property
+    def mimeType(self):
+        '''The file format media type.'''
+        return None
+
+    @property
+    def info(self):
+        '''The file format reference info.'''
+        return 'https://en.wikipedia.org/wiki/Embedded_database#Firebird_Embedded'
+
+    def isBinary(self):
+        '''Tells whether the file format is binary or not.'''
+        return True
+
+    def isExportable(self):
+        '''Tells whether the file format is exportable or not.'''
+        return True
