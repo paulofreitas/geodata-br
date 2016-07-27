8a43dbd05ac33c874277171b49b0206bb3eb36cf
Paulo Freitas <me@paulofreitas.me>
2016-07-27 02:18:03 -0300

    Added a new core constants module

---
+++ src/dtb/core/constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2016 Paulo Freitas
+# MIT License (see LICENSE file)
+'''
+Core constants module
+
+This module provides the core constants.
+'''
+# Imports
+
+# Built-in dependencies
+
+from os.path import abspath, dirname, join as path
+
+# Constants
+
+PKG_DIR = abspath(path(dirname(__file__), '..'))
+SRC_DIR = abspath(path(PKG_DIR, '..'))
+BASE_DIR = abspath(path(SRC_DIR, '..'))
+DATA_DIR = path(BASE_DIR, 'data')
