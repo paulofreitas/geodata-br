632fc68e21d67571787f592d9eeab8eae407a490
Paulo Freitas <me@paulofreitas.me>
2016-07-28 01:04:17 -0300

    Improved core types module
    - Added a new Bytes class providing an improved bytes() type with support for unpacking binary data;
    - Added a new Struct.__getitem__() method to proxy nested dictionaries;
    - Added a new Struct.__repr__() method to provide better string representation of Struct objects;
    - Added a new Struct.toJSON() method to serialize Struct objects to JSON;
    - Fixed Struct.__delattr__() method to raise an AttributeError when a given key is not found;
    - Simplified Struct.copy() implementation;
    - Greatly improved Struct methods docstrings;
    - Updated imports.

--- src/dtb/core/types.py
+++ src/dtb/core/types.py
@@ -11,6 +11,10 @@ This module provides the base types used across the packages.
 
 # Built-in dependencies
 
+import json
+
+from struct import Struct as _Struct
+
 from abc import ABCMeta
 
 # External compatibility dependencies
@@ -52,13 +56,55 @@ class AbstractClass(object, with_metaclass(ABCMeta)):
         return type.__subclasses__(cls)
 
 
+class Bytes(bytes):
+    '''An improved bytes type.'''
+
+    def unpack(self, _format):
+        '''Unpacks data from a binary string according to the given format.
+
+        Arguments:
+            _format (str): The pack format
+        '''
+        compiled_format = _Struct(_format)
+
+        return compiled_format.unpack_from(self) \
+            + tuple(self[compiled_format.size:])
+
+
 class Struct(dict):
-    '''A dictionary object which can access/assign/delete keys with attributes.'''
+    '''An improved dictionary that provides attribute-style access to keys.'''
+
+    def __getitem__(self, key):
+        '''Magic method to proxy nested dictionaries.
+
+        Arguments:
+            key (str): The dictionary key to access
+
+        Returns:
+            The dictionary key
+
+        Raises:
+            KeyError: When a given key is not found
+        '''
+        value = super(self.__class__, self).__getitem__(key)
+
+        if isinstance(value, dict):
+            return self.__class__(value)
+
+        return value
 
     def __getattr__(self, key):
         '''Magic method to allow accessing dictionary keys as attributes.
 
-        :param key: the dictionary key to access'''
+        Arguments:
+            key (str): The dictionary key to access
+
+        Returns:
+            The dictionary key
+
+        Raises:
+            AttributeError: When a given key is not found
+        '''
         try:
             return self[key]
         except KeyError:
@@ -67,16 +113,49 @@ class Struct(dict):
     def __setattr__(self, key, value):
         '''Magic method to allow assigning dictionary keys with attributes.
 
-        :param key: the dictionary key to change
-        :param value: the value to set'''
+        Arguments:
+            key (str): The dictionary key to change
+            value: The value to set
+        '''
         self[key] = value
 
     def __delattr__(self, key):
         '''Magic method to allow deleting dictionary keys with attributes.
 
-        :param key: the dictionary key to delete'''
-        del self[key]
+        Arguments:
+            key (str): The dictionary key to delete
+
+        Raises:
+            AttributeError: When a given key is not found
+        '''
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __repr__(self):
+        '''Returns a string representation of this object.
+
+        Returns:
+            str: A string representation of this object
+        '''
+        return '{}({})'.format(self.__class__.__name__,
+                               super(self.__class__, self).__repr__())
 
     def copy(self):
-        '''Copies the self data into a new Struct instance.'''
-        return self.__class__(dict.copy(self))
+        '''Copies the self data into a new Struct instance.
+
+        Returns:
+            Struct: A new Struct instance with this instance data
+        '''
+        return self.__class__(self)
+
+    # Serialization methods
+
+    def toJSON(self):
+        '''Serializes this object into a JSON string.
+
+        Returns:
+            str: A JSON representation of this object
+        '''
+        return json.dumps(self)
