19ae12870cebc95fe074b1de22cf210a81726313
Paulo Freitas <me@paulofreitas.me>
2016-07-28 03:34:38 -0300

    Greatly improved commands package
    - Improved package docstring;
    - Renamed CliParser class to Command;
    - Tweaked Command class to extends the new AbstractClass type;
    - Cleaned up Command constructor method removing unused metadata argument;
    - Tweaked Command constructor method to use the new Command.usage, Command.description and Command.epilog properties on parser initialization;
    - Tweaked Command.setDefaults() method to use the new Command.version property;
    - Added new Command.usage(), Command.description(), Command.epilog() and Command.version() property methods;
    - Tweaked Command.configure() method to raise an NotImplementedError;
    - Added a new Command.run() soft-abstract method;
    - Added all missing docstrings;
    - Updated imports;
    - Minor linting formatting fixes.

--- src/dtb/commands/__init__.py
+++ src/dtb/commands/__init__.py
@@ -3,10 +3,9 @@
 # Copyright (c) 2013-2016 Paulo Freitas
 # MIT License (see LICENSE file)
 '''
-CLI package
+Commands package
 
-This package provides a base CLI argument parser class and concrete command
-modules.
+This package provides a base command class and concrete command modules.
 '''
 # Imports
 
@@ -15,6 +14,10 @@ modules.
 import argparse
 import logging
 import sys
+
+# Package dependencies
+
+from dtb.core.types import AbstractClass
 
 # Package metadata
 
@@ -26,10 +29,14 @@ __license__ = 'MIT License'
 # Classes
 
 
-class CliParser(object):
-    def __init__(self, **metadata):
-        self._metadata = metadata
-        self._parser = argparse.ArgumentParser(**metadata)
+class Command(AbstractClass):
+    '''Abstract command class.'''
+
+    def __init__(self):
+        '''Constructor.'''
+        self._parser = argparse.ArgumentParser(usage=self.usage,
+                                               description=self.description,
+                                               epilog=self.epilog)
         self._parser.conflict_handler = 'resolve'
         self._parser.formatter_class = argparse.RawTextHelpFormatter
         self._arguments = {}
@@ -38,31 +45,32 @@ class CliParser(object):
         self.setLogging()
 
     def addArgumentGroup(self, key, name):
+        '''Adds a new argument group.'''
         self._arguments[key] = self._parser.add_argument_group(name)
 
     def addArgument(self, group, *args, **kwargs):
+        '''Adds a new argument to the given group.'''
         self._arguments[group].add_argument(*args, **kwargs)
 
     def setDefaults(self):
+        '''Sets the command default arguments.'''
         self.addArgumentGroup('global', 'Global options')
         self.addArgument('global',
                          '-h', '--help',
                          action='help',
-                         help='Display this information'
-        )
+                         help='Display this information')
         self.addArgument('global',
                          '-v', '--version',
                          action='version',
-                         version='%(prog)s ' + self._metadata.get('__version__', ''),
-                         help='Show version information and exit'
-        )
+                         version='%(prog)s ' + self.version,
+                         help='Show version information and exit')
         self.addArgument('global',
                          '-V', '--verbose',
                          action='store_true',
-                         help='Display informational messages and warnings'
-        )
+                         help='Display informational messages and warnings')
 
     def setLogging(self):
+        '''Sets the command logging handlers.'''
         # Setup stream handler
         stream_formatter = logging.Formatter('%(message)s')
         stream_handler = logging.StreamHandler(sys.stdout)
@@ -72,10 +80,32 @@ class CliParser(object):
         self._logger.addHandler(stream_handler)
         self._logger.setLevel(logging.INFO)
 
+    @property
+    def usage(self):
+        '''Defines the command usage syntax.'''
+        return None
+
+    @property
+    def description(self):
+        '''Defines the command description.'''
+        return None
+
+    @property
+    def epilog(self):
+        '''Defines the command epilog message.'''
+        return None
+
+    @property
+    def version(self):
+        '''Defines the command version.'''
+        return __version__
+
     def configure(self):
-        pass
+        '''Configures the command arguments.'''
+        raise NotImplementedError
 
     def parse(self):
+        '''Parses the given command arguments.'''
         self.configure()
 
         args = self._parser.parse_args()
@@ -84,3 +114,7 @@ class CliParser(object):
             self._logger.setLevel(logging.DEBUG)
 
         return args
+
+    def run(self):
+        '''Runs the command.'''
+        raise NotImplementedError
