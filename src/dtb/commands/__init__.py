cc1a440fa73bf36d7405961043441fbc8b84c53e
Paulo Freitas <me@paulofreitas.me>
2016-07-29 02:58:23 -0300

    Further greatly improved commands package
    - Improved package docstring;
    - Added a new Application class to use Command class instances as sub-commands;
    - Added a new HelpFormatter class to customize the parser output;
    - Changed Command constructor method to require a new application argument;
    - Improved Command constructor method to register the command parser into the application instance, simplified the parser configuration;
    - Tweaked Command constructor method to use the new HelpFormatter class as the parser formatter class;
    - Removed unused Command._arguments attribute;
    - Removed unused Command.addArgumentGroup() method;
    - Simplified Command.setDefaults() method for design changes;
    - Simplified Command.setLogging() method removing unneeded stuff;
    - Tweaked Command.setLogging() method to use the command class name as logger name;
    - Simplified Command.addArgument() method removing the unused group argument, changed to add argument into the command parser;
    - Added new Command.name() and Command.help() property methods;
    - Removed unused Command.version() property method;
    - Changed Command.configure() method to be no-op by default;
    - Removed unused Command.parse() method;
    - Renamed Command.run() method to Command.handle();
    - Improved Command methods docstrings.

--- src/dtb/commands/__init__.py
+++ src/dtb/commands/__init__.py
@@ -5,7 +5,8 @@
 '''
 Commands package
 
-This package provides a base command class and concrete command modules.
+This package provides an application class, an abstract command class and
+concrete command modules.
 '''
 # Imports
 
@@ -28,86 +29,97 @@ __license__ = 'MIT License'
 
 # Classes
 
-
-class Command(AbstractClass):
-    '''Abstract command class.'''
+class Application(object):
+    '''Main application class.'''
 
     def __init__(self):
         '''Constructor.'''
-        self._parser = argparse.ArgumentParser(usage=self.usage,
-                                               description=self.description,
-                                               epilog=self.epilog)
-        self._parser.conflict_handler = 'resolve'
-        self._parser.formatter_class = argparse.RawTextHelpFormatter
-        self._arguments = {}
+        self._parser = argparse.ArgumentParser(
+            description=self.description,
+            epilog=self.epilog,
+            add_help=False,
+            conflict_handler='resolve',
+            formatter_class=HelpFormatter)
+        self._subparsers = self._parser.add_subparsers(
+            title='Commands',
+            dest='command',
+            metavar='')
+        self._commands = {}
 
         self.setDefaults()
         self.setLogging()
 
-    def addArgumentGroup(self, key, name):
-        '''Adds a new argument group.'''
-        self._arguments[key] = self._parser.add_argument_group(name)
-
-    def addArgument(self, group, *args, **kwargs):
-        '''Adds a new argument to the given group.'''
-        self._arguments[group].add_argument(*args, **kwargs)
-
     def setDefaults(self):
-        '''Sets the command default arguments.'''
-        self.addArgumentGroup('global', 'Global options')
-        self.addArgument('global',
+        '''Sets the application default arguments.'''
+        default_args = self.addArgumentGroup('Arguments')
+
+        self.addArgument(default_args,
                          '-h', '--help',
                          action='help',
                          help='Display this information')
-        self.addArgument('global',
+        self.addArgument(default_args,
                          '-v', '--version',
                          action='version',
                          version='%(prog)s ' + self.version,
                          help='Show version information and exit')
-        self.addArgument('global',
+        self.addArgument(default_args,
                          '-V', '--verbose',
                          action='store_true',
                          help='Display informational messages and warnings')
 
     def setLogging(self):
-        '''Sets the command logging handlers.'''
-        # Setup stream handler
-        stream_formatter = logging.Formatter('%(message)s')
-        stream_handler = logging.StreamHandler(sys.stdout)
-        stream_handler.setFormatter(stream_formatter)
-
-        self._logger = logging.getLogger('dtb')
-        self._logger.addHandler(stream_handler)
+        '''Sets the application logging handlers.'''
+        self._logger = logging.getLogger(self.__class__.__name__)
         self._logger.setLevel(logging.INFO)
 
-    @property
-    def usage(self):
-        '''Defines the command usage syntax.'''
-        return None
+        # Setup stream handler
+        self._logger.addHandler(logging.StreamHandler(sys.stdout))
 
-    @property
-    def description(self):
-        '''Defines the command description.'''
-        return None
+    def addParser(self, *args, **kwargs):
+        '''Adds a new command parser to the application.
 
-    @property
-    def epilog(self):
-        '''Defines the command epilog message.'''
-        return None
+        Returns:
+            argparse.ArgumentParser: The command argument parser
+        '''
+        return self._subparsers.add_parser(*args, **kwargs)
 
-    @property
-    def version(self):
-        '''Defines the command version.'''
-        return __version__
+    def addCommand(self, command_class):
+        '''Adds a new command to the application.
 
-    def configure(self):
-        '''Configures the command arguments.'''
-        raise NotImplementedError
+        Arguments:
+            command_class (Command): The command class
+        '''
+        command = command_class(self)
+        command.configure()
+
+        self._commands[command.name] = command
+
+    def addArgumentGroup(self, title):
+        '''Adds a new argument group.
+
+        Arguments:
+            title (str): The argument group title
+
+        Returns:
+            argparse._ArgumentGroup: The argument group instance
+        '''
+        return self._parser.add_argument_group(title)
+
+    def addArgument(self, group, *args, **kwargs):
+        '''Adds a new argument to the given argument group.
+
+        Arguments:
+            group (argparse._ArgumentGroup): The argument group
+        '''
+        group.add_argument(*args, **kwargs)
+
+    def registerCommands(self):
+        '''Registers the available command modules into the application.'''
+        for command in Command.childs():
+            self.addCommand(command)
 
     def parse(self):
-        '''Parses the given command arguments.'''
-        self.configure()
-
+        '''Parses the given application arguments.'''
         args = self._parser.parse_args()
 
         if args.verbose:
@@ -116,5 +128,108 @@ class Command(AbstractClass):
         return args
 
     def run(self):
-        '''Runs the command.'''
+        '''Runs the application.'''
+        self.registerCommands()
+
+        args = self.parse()
+        command = self._commands.get(args.command)
+
+        if command:
+            command.handle(args)
+
+    @property
+    def version(self):
+        '''Defines the application version.'''
+        return __version__
+
+    @property
+    def description(self):
+        '''Defines the application description.'''
+        return 'Brazilian territorial data exporter'''
+
+    @property
+    def epilog(self):
+        '''Defines the application epilog message.'''
+        return 'Report bugs and feature requests to {}.' \
+            .format('https://github.com/paulofreitas/dtb-ibge/issues')
+
+
+class Command(AbstractClass):
+    '''Abstract command class.'''
+
+    def __init__(self, application):
+        '''Constructor.
+
+        Arguments:
+            application (Application): The application class instance
+        '''
+        self._parser = application.addParser(
+            self.name,
+            help=self.description,
+            usage=self.usage,
+            description=self.description,
+            epilog=self.epilog,
+            conflict_handler='resolve',
+            formatter_class=HelpFormatter)
+
+        self.setDefaults()
+        self.setLogging()
+
+    def setDefaults(self):
+        '''Sets the command default arguments.'''
+        self.addArgument('-h', '--help',
+                         action='help',
+                         help='Display this information')
+
+    def setLogging(self):
+        '''Sets the command logging handlers.'''
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._logger.setLevel(logging.INFO)
+
+        # Setup stream handler
+        self._logger.addHandler(logging.StreamHandler(sys.stdout))
+
+    def addArgument(self, *args, **kwargs):
+        '''Adds a new argument to the command.'''
+        self._parser.add_argument(*args, **kwargs)
+
+    @property
+    def name(self):
+        '''Defines the command name.'''
         raise NotImplementedError
+
+    @property
+    def description(self):
+        '''Defines the command description.'''
+        return None
+
+    @property
+    def usage(self):
+        '''Defines the command usage syntax.'''
+        return None
+
+    @property
+    def epilog(self):
+        '''Defines the command epilog message.'''
+        return None
+
+    def configure(self):
+        '''Defines the command arguments.'''
+        pass
+
+    def handle(self, args):
+        '''Handles the command.
+
+        Arguments:
+            args (argparser.Namespace): The command arguments
+        '''
+        raise NotImplementedError
+
+
+class HelpFormatter(argparse.HelpFormatter):
+    '''Custom help formatter class to use when parsing arguments.'''
+
+    def start_section(self, heading):
+        heading = heading.replace('optional arguments', 'Arguments')
+
+        return super(self.__class__, self).start_section(heading)
