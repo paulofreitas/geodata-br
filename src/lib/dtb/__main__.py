1d36dbaac77419ca688d50367ca5bcbc9c04d8ca
Paulo Freitas <me@paulofreitas.me>
2016-07-29 23:37:25 -0300

    Added a new main module to be used as a console script

---
+++ src/lib/dtb/__main__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2016 Paulo Freitas
+# MIT License (see LICENSE file)
+'''
+Main console script
+'''
+# Imports
+
+# Package dependencies
+
+from dtb.commands import Application
+
+# Functions
+
+def main():
+    '''Main entry point.'''
+    app = Application()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
