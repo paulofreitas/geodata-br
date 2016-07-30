6e0394427a967acb5319431056ccd01551472829
Paulo Freitas <me@paulofreitas.me>
2016-07-29 23:38:41 -0300

    Significantly improved package setup script
    - Added brief copyright and license info as comments;
    - Improved module docstring to be more informative;
    - Added Python version compatibility checking;
    - Fixed setup() version argument to use semantic version;
    - Tweaked setup() packages argument to use the setuptools.find_packages() method to lookup packages under the new lib/ directory;
    - Added a new setup() package_dir argument to set packages directory to the new lib/ directory;
    - Added a new setup() entry_points argument to provide a new dtb console script;
    - Added new setup() python_requires and install_requires arguments to list all package dependencies;
    - Updated imports.

--- src/setup.py
+++ src/setup.py
@@ -1,46 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''Brazilian territorial distribution data exporter
-
-The MIT License (MIT)
-
-Copyright (c) 2013-2016 Paulo Freitas
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+# Copyright (c) 2013-2016 Paulo Freitas
+# MIT License (see LICENSE file)
 '''
+Setup script
+'''
+# Imports
+
+# Built-in dependencies
+
+import sys
+
 from setuptools import setup, find_packages
+
+# Compatibility check
+
+if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[:2] < (3, 2):
+    raise RuntimeError('Python version 2.7 or >= 3.2 required')
+
+# Routines
 
 setup(
     # Package metadata
     name='dtb',
-    version='1.0-dev',
-    license='MIT',
+    version='1.0.0-dev',
     description='Brazilian territorial distribution data exporter',
+    license='MIT',
     url='https://github.com/paulofreitas/dtb-ibge',
     author='Paulo Freitas',
     author_email='me@paulofreitas.me',
 
     # Package distribution
-    packages=['dtb'],
-    scripts=[
-        'dtb/commands/build.py',
-        'dtb/commands/build_docs.py',
-        'dtb/commands/export.py'
-    ],
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    entry_points={
+        'console_scripts': [
+            'dtb = dtb.__main__:main'
+        ],
+    },
+
+    # Package dependencies
+    python_requires='>=2.7,!=3.0.*,!=3.1.*',
+    install_requires=[
+        # dtb package
+        'future',
+        # dtb.exporters package
+        'fdb',
+        'lxml',
+        'phpserialize',
+        'pyyaml',
+        'sqlalchemy',
+        # dtb.parsers package
+        'xlrd',
+    ]
 )
