6ec6922773b680b5862ced79455f92afc9bfcecb
Paulo Freitas <me@paulofreitas.me>
2016-07-30 08:16:40 -0300

    Greatly improved databases package
    - Improved package docstring;
    - Added a new DatabaseFactory class to ease retrieving databases instances;
    - Added a new DatabaseRepository class to ease retrieving databases info;
    - Added new DatabaseError and UnknownDatabaseError exception classes;
    - Added a new module-wide logger variable;
    - Improved BASE_LIST constant to list the database's infos in the package itself avoiding the use of an external file;
    - Removed the hardcoded Database.base_list and Database.bases attributes;
    - Simplified Database constructor method removing the unneeded logger argument/attribute and exception stuff;
    - Changed Database constructor method to use an info argument instead;
    - Tweaked Database methods to use the module logger instance;
    - Added missing Database constructor method docstring;
    - Cleaned up unused imports;
    - Other minor simplifications.

--- src/lib/dtb/databases/__init__.py
+++ src/lib/dtb/databases/__init__.py
@@ -5,7 +5,8 @@
 '''
 Databases package
 
-This package provides a database class.
+This package provides a database class, a database factory class and a database
+repository class.
 '''
 from __future__ import absolute_import, unicode_literals
 
@@ -20,16 +21,13 @@ import zipfile
 
 from io import open
 from os import makedirs
-from os.path import basename, dirname, exists, join as path
-
-# External dependencies
-
-import yaml
+from os.path import dirname, exists, join as path
 
 # Package dependencies
 
-from dtb.core.constants import PKG_DIR, SRC_DIR
+from dtb.core.constants import SRC_DIR
 from dtb.core.entities import TerritorialData
+from dtb.core.logging import Logger
 from dtb.core.types import Bytes, Struct
 from dtb.exporters import ExporterFactory
 from dtb.parsers import ParserFactory
@@ -41,26 +39,84 @@ __author__ = 'Paulo Freitas <me@paulofreitas.me>'
 __copyright__ = 'Copyright (c) 2013-2016 Paulo Freitas'
 __license__ = 'MIT License'
 
+# Module logging
+
+logger = Logger.instance(__name__)
+
 # Constants
 
-BASE_LIST = path(PKG_DIR, 'data/bases.yaml')
+BASE_LIST = [
+    Struct(year=2014,
+           archive='dtb_2014_v2.zip',
+           file='DTB_2014_v2/DTB_2014_subdistrito.xls',
+           format='xls',
+           sheet='Plan1'),
+    Struct(year=2013,
+           archive='dtb_2013.zip',
+           file='dtb_2013.xls',
+           format='xls',
+           sheet='dtb_2013'),
+    Struct(year=2012,
+           archive='dtb_2012.zip',
+           file='dtb_2012.xls',
+           format='xls',
+           sheet='Estrutura_2012___Município'),
+    Struct(year=2011,
+           archive='dtb_2011.zip',
+           file='dtb_2011.xls',
+           format='xls',
+           sheet='DTB_2011'),
+    Struct(year=2010,
+           archive='dtb_2010.zip',
+           file='dtb_2010.xls',
+           format='xls',
+           sheet='Município'),
+    Struct(year=2009,
+           archive='dtb_05_05_2009.zip',
+           file='DTB_05_05_2009.xls',
+           format='xls',
+           sheet='DTB_05_05_2009n'),
+    Struct(year=2008,
+           archive='dtb_2008.zip',
+           file='DTB_2008.xls',
+           format='xls',
+           sheet='DTB_Nome_Comum'),
+    Struct(year=2007,
+           archive='dtb_2007.zip',
+           file='DTB_2007.xls',
+           format='xls',
+           sheet='DTB_Nome_Comum'),
+    Struct(year=2006,
+           archive='dtb_2006.zip',
+           file='DTB_2006.xls',
+           format='xls',
+           sheet='Completo'),
+    Struct(year=2005,
+           archive='dtb_2005.zip',
+           file='DTB_2005.xls',
+           format='xls',
+           sheet='NomeNormal'),
+    Struct(year=2003,
+           archive='dtb17112003nome.zip',
+           file='DTB17112003nome.xls',
+           format='xls',
+           sheet='DTBnome'),
+]
 
 # Classes
 
 
 class Database(object):
     '''Database class.'''
-    base_list = dict((str(database['year']), database)
-                     for database in yaml.load(open(BASE_LIST)))
-    bases = tuple(reversed(sorted(base_list.keys())))
 
-    def __init__(self, year, logger):
-        if year not in self.bases:
-            raise Exception('This base is not available to download.')
+    def __init__(self, info):
+        '''Constructor.
 
-        self._info = Struct(self.base_list.get(year))
+        Arguments:
+            info (Struct): The database info
+        '''
+        self._info = info
         self._data = TerritorialData(self._info)
-        self._logger = logger
 
     @property
     def year(self):
@@ -98,24 +154,23 @@ class Database(object):
         archive_data = io.BytesIO()
         base_data = io.BytesIO()
 
-        self._logger.debug('Connecting to FTP server...')
+        logger.debug('Connecting to FTP server...')
         ftp.connect()
 
-        self._logger.debug('Logging into the FTP server...')
+        logger.debug('Logging into the FTP server...')
         ftp.login()
         ftp.cwd(path('organizacao_do_territorio',
                      'estrutura_territorial',
                      'divisao_territorial',
                      self.year))
 
-        self._logger.info('Retrieving database...')
+        logger.info('Retrieving database...')
 
         if self.archive:
-            ftp.retrbinary('RETR {}'.format(basename(self.archive)),
-                           archive_data.write)
+            ftp.retrbinary('RETR {}'.format(self.archive), archive_data.write)
 
             with zipfile.ZipFile(archive_data, 'r') as archive_file:
-                self._logger.info('Reading database...')
+                logger.info('Reading database...')
 
                 with archive_file.open(self.file, 'r') as base_file:
                     base_data.write(base_file.read())
@@ -150,10 +205,10 @@ class Database(object):
         '''Parses the given database.'''
         parser = ParserFactory.fromFormat(self.format)
 
-        try:
-            self._data = parser(self, self._logger).parse()
-        except:
-            raise Exception('Failed to parse data using the given parser')
+        #try:
+        self._data = parser(self).parse()
+        #except:
+        #    raise Exception('Failed to parse data using the given parser')
 
         return self
 
@@ -169,10 +224,10 @@ class Database(object):
         export_format = exporter._format()
 
         if minified:
-            self._logger.info('Exporting database to minified {} format...' \
+            logger.info('Exporting database to minified {} format...' \
                 .format(export_format.friendlyName))
         else:
-            self._logger.info('Exporting database to {} format...' \
+            logger.info('Exporting database to {} format...' \
                 .format(export_format.friendlyName))
 
         data = exporter(self._data, minified).data
@@ -180,7 +235,7 @@ class Database(object):
         if not export_format.isBinary() and not isinstance(data, unicode):
             data = unicode(data.decode('utf-8'))
 
-        self._logger.debug('Done.')
+        logger.debug('Done.')
 
         if filename:
             if filename == 'auto':
@@ -192,3 +247,55 @@ class Database(object):
                 export_file.write(data)
         else:
             sys.stdout.write(data)
+
+
+class DatabaseFactory(object):
+    '''Database factory class.'''
+
+    @classmethod
+    def fromYear(cls, year):
+        '''Factories a database class for a given database year.
+
+        Arguments:
+            year (int): The database year to retrieve a database object
+
+        Raises:
+            UnknownDatabaseError: If no database is found with the given year
+        '''
+        return DatabaseRepository.findByYear(year)
+
+
+class DatabaseRepository(object):
+    '''Database repository class.'''
+
+    @staticmethod
+    def findByYear(year):
+        '''Returns the database for the given year.
+
+        Arguments:
+            year (int): The database year
+
+        Raises:
+            UnknownDatabaseError: If no database is found with the given year
+        '''
+        for database_info in BASE_LIST:
+            if database_info.year == int(year):
+                return Database(database_info)
+
+        raise UnknownDatabaseError('This base is not available: {}' \
+                                       .format(year))
+
+    @staticmethod
+    def listYears():
+        '''Returns a list with all database years.'''
+        return [str(database.year) for database in BASE_LIST]
+
+
+class DatabaseError(Exception):
+    '''Generic exception class for database errors.'''
+    pass
+
+
+class UnknownDatabaseError(DatabaseError):
+    '''Exception class raised when a given database is not found.'''
+    pass
