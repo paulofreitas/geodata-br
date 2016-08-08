c3e52181d71c8197eba9ddeb01c358462c67ae9a
Paulo Freitas <me@paulofreitas.me>
2016-08-08 06:06:26 -0300

    Improved yaml exporter module
    - Moved YAML dumping customizations into a new YAML format utils module;
    - Replaced YamlExporter.data() property method with a new YamlExporter.export() method following the interface changes;
    - Improved YamlExporter.export() method to build smaller minified files;
    - Improved docstrings;
    - Updated/cleaned up imports.

--- src/lib/dtb/exporters/yaml.py
+++ src/lib/dtb/exporters/yaml.py
@@ -5,13 +5,14 @@
 '''
 YAML file exporter module
 '''
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 # Imports
 
 # Built-in dependencies
 
-import collections
+import io
+import re
 
 # External dependencies
 
@@ -21,75 +22,43 @@ import yaml
 
 from dtb.exporters import Exporter
 from dtb.formats.yaml import YamlFormat
-
-# Enhancements
-
-
-def __represent_odict(dump, tag, mapping, flow_style=None):
-    '''Make PyYAML output an OrderedDict.
-    Credits: https://gist.github.com/miracle2k/3184458/
-    '''
-    value = []
-    node = yaml.MappingNode(tag, value, flow_style=flow_style)
-
-    if dump.alias_key is not None:
-        dump.represented_objects[dump.alias_key] = node
-
-    best_style = True
-
-    if hasattr(mapping, 'items'):
-        mapping = mapping.items()
-
-    for item_key, item_value in mapping:
-        node_key = dump.represent_data(item_key)
-        node_value = dump.represent_data(item_value)
-
-        if not (isinstance(node_key, yaml.ScalarNode) and not node_key.style):
-            best_style = False
-
-        if not (isinstance(node_value, yaml.ScalarNode)
-                and not node_value.style):
-            best_style = False
-
-        value.append((node_key, node_value))
-
-        if flow_style is None:
-            if dump.default_flow_style is not None:
-                node.flow_style = dump.default_flow_style
-            else:
-                node.flow_style = best_style
-
-    return node
-
-
-yaml.SafeDumper.add_representer(
-    collections.OrderedDict,
-    lambda dumper, value:
-        __represent_odict(dumper, 'tag:yaml.org,2002:map', value)
-)
-
+from dtb.formats.yaml.utils import OrderedDumper
 
 # Classes
 
 
 class YamlExporter(Exporter):
-    '''YAML exporter class.'''
+    '''
+    YAML exporter class.
+    '''
 
     # Exporter format
-    _format= YamlFormat
+    _format = YamlFormat
 
-    @property
-    def data(self):
-        '''Formatted YAML representation of data.'''
-        data = self._data.toDict()
-        yaml_opts = dict(default_flow_style=False)
+    def export(self, **options):
+        '''
+        Exports the data into a YAML file-like stream.
 
-        if self._minified:
-            yaml_opts.update(default_flow_style=True, width=2e6, indent=0)
+        Arguments:
+            options (dict): The exporting options
 
-        yaml_str = yaml.safe_dump(data, **yaml_opts)
+        Returns:
+            io.BytesIO: A YAML file-like stream
 
-        if self._minified:
-            yaml_str = yaml_str.replace('}, ', '},')
+        Raises:
+            ExportError: When data fails to export
+        '''
+        yaml_options = dict(default_flow_style=False)
 
-        return yaml_str
+        if options.get('minify'):
+            yaml_options.update(default_flow_style=True,
+                                width=2e6,
+                                indent=0)
+
+        data = self._data.normalize(forceUnicode=True)
+        yaml_data = yaml.dump(data, Dumper=OrderedDumper, **yaml_options)
+
+        if options.get('minify'):
+            yaml_data = re.sub('(?<=[,:])\s+', '', yaml_data)
+
+        return io.BytesIO(yaml_data)
