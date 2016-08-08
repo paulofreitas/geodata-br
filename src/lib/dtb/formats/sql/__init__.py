61e38d7cd873ec53e8154a191183936df890835e
Paulo Freitas <me@paulofreitas.me>
2016-08-08 06:11:21 -0300

    Improved sql format package
    - Changed SqlFormat properties to use the new @classproperty decorator;
    - Changed SqlFormat.isExportable() and SqlFormat.isMinifiable() methods to property methods using the new @classproperty decorator;
    - Improved docstrings formatting;
    - Updated imports.

--- src/lib/dtb/formats/sql/__init__.py
+++ src/lib/dtb/formats/sql/__init__.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2013-2016 Paulo Freitas
 # MIT License (see LICENSE file)
 '''
-SQL file format module
+SQL file format package
 '''
 from __future__ import absolute_import
 
@@ -11,48 +11,69 @@ from __future__ import absolute_import
 
 # Package dependencies
 
+from dtb.core.helpers.decorators import classproperty
 from dtb.formats import Format
 
 # Classes
 
 
 class SqlFormat(Format):
-    '''The file format class for SQL file format.'''
+    '''
+    The file format class for SQL file format.
+    '''
 
-    @property
+    @classproperty
     def name(self):
-        '''The file format name.'''
+        '''
+        The file format name.
+        '''
         return 'sql'
 
-    @property
+    @classproperty
     def friendlyName(self):
-        '''The file format friendly name.'''
+        '''
+        The file format friendly name.
+        '''
         return 'SQL'
 
-    @property
+    @classproperty
     def extension(self):
-        '''The file format extension.'''
+        '''
+        The file format extension.
+        '''
         return '.sql'
 
-    @property
+    @classproperty
     def type(self):
-        '''The file format type.'''
+        '''
+        The file format type.
+        '''
         return 'Database'
 
-    @property
+    @classproperty
     def mimeType(self):
-        '''The file format media type.'''
+        '''
+        The file format media type.
+        '''
         return 'application/sql'
 
-    @property
+    @classproperty
     def info(self):
-        '''The file format reference info.'''
+        '''
+        The file format reference info.
+        '''
         return 'https://en.wikipedia.org/wiki/SQL'
 
+    @classproperty
     def isExportable(self):
-        '''Tells whether the file format is exportable or not.'''
+        '''
+        Tells whether the file format is exportable or not.
+        '''
         return True
 
+    @classproperty
     def isMinifiable(self):
-        '''Tells whether the file format is minifiable or not.'''
+        '''
+        Tells whether the file format is minifiable or not.
+        '''
         return True
