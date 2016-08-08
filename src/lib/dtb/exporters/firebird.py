d4af87d75693009dc50cdc3ca64ddf809b5aa8a7
Paulo Freitas <me@paulofreitas.me>
2016-08-08 15:25:13 -0300

    Improved firebird exporter module
    - Replaced FirebirdExporter.data() property method with a new FirebirdExporter.export() method following the interface changes;
    - Improved docstrings;
    - Updated imports.

--- src/lib/dtb/exporters/firebird.py
+++ src/lib/dtb/exporters/firebird.py
@@ -15,6 +15,7 @@ import tempfile
 
 # External dependencies
 
+import io
 import fdb
 
 # Package dependencies
@@ -27,37 +28,49 @@ from dtb.formats.firebird import FirebirdFormat
 
 
 class FirebirdExporter(Exporter):
-    '''Firebird exporter class.'''
+    '''
+    Firebird exporter class.
+    '''
 
     # Exporter format
     _format = FirebirdFormat
 
-    @property
-    def data(self):
-        sql_str = SqlExporter(self._data,
-                              self._minified,
-                              dialect='firebird').data
+    def export(self, **options):
+        '''
+        Exports the data into a Firebird Embedded file-like stream.
 
-        _fdb_file = tempfile.mktemp()
-        db = fdb.create_database(
+        Arguments:
+            options (dict): The exporting options
+
+        Returns:
+            io.BytesIO: A Firebird Embedded file-like stream
+
+        Raises:
+            ExportError: When data fails to export
+        '''
+        sql_options = dict(options, dialect='firebird')
+        sql_data = SqlExporter(self._data).export(**sql_options)
+        fdb_data = io.BytesIO()
+        fdb_file = tempfile.mktemp()
+        fdb_con = fdb.create_database(
             "CREATE DATABASE '{}' USER '{}' PASSWORD '{}'" \
-                .format(_fdb_file, 'sysdba', 'masterkey'),
+                .format(fdb_file, 'sysdba', 'masterkey'),
             sql_dialect=3
         )
-        cursor = db.cursor()
+        fdb_cursor = fdb_con.cursor()
         in_trans = False
 
-        for stmt in sql_str.rstrip(';').split(';'):
+        for stmt in sql_data.read().rstrip(';').split(';'):
             if stmt.startswith('INSERT') and not in_trans:
-                db.begin()
+                fdb_con.begin()
                 in_trans = True
             else:
-                db.commit()
+                fdb_con.commit()
                 in_trans = False
 
-            cursor.execute(stmt)
+            fdb_cursor.execute(stmt)
 
-        with open(_fdb_file, 'rb') as fdb_file:
-            fdb_data = fdb_file.read()
+        fdb_data.write(fdb_file.read())
+        fdb_data.seek(0)
 
         return fdb_data
