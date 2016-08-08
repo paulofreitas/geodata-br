977e087ba026b35412bf71a19bddb2856c1f3e19
Paulo Freitas <me@paulofreitas.me>
2016-08-08 05:35:28 -0300

    Improved json format module
    - Changed JsonFormat properties to use the new @classproperty decorator;
    - Changed JsonFormat.isExportable() and JsonFormat.isMinifiable() methods to property methods using the new @classproperty decorator;
    - Improved docstrings formatting;
    - Updated imports.

--- src/lib/dtb/formats/json.py
+++ src/lib/dtb/formats/json.py
@@ -11,48 +11,69 @@ from __future__ import absolute_import
 
 # Package dependencies
 
+from dtb.core.helpers.decorators import classproperty
 from dtb.formats import Format
 
 # Classes
 
 
 class JsonFormat(Format):
-    '''The file format class for JSON file format.'''
+    '''
+    The file format class for JSON file format.
+    '''
 
-    @property
+    @classproperty
     def name(self):
-        '''The file format name.'''
+        '''
+        The file format name.
+        '''
         return 'json'
 
-    @property
+    @classproperty
     def friendlyName(self):
-        '''The file format friendly name.'''
+        '''
+        The file format friendly name.
+        '''
         return 'JSON'
 
-    @property
+    @classproperty
     def extension(self):
-        '''The file format extension.'''
+        '''
+        The file format extension.
+        '''
         return '.json'
 
-    @property
+    @classproperty
     def type(self):
-        '''The file format type.'''
+        '''
+        The file format type.
+        '''
         return 'Data Interchange'
 
-    @property
+    @classproperty
     def mimeType(self):
-        '''The file format media type.'''
+        '''
+        The file format media type.
+        '''
         return 'application/json'
 
-    @property
+    @classproperty
     def info(self):
-        '''The file format reference info.'''
+        '''
+        The file format reference info.
+        '''
         return 'https://en.wikipedia.org/wiki/JSON'
 
+    @classproperty
     def isExportable(self):
-        '''Tells whether the file format is exportable or not.'''
+        '''
+        Tells whether the file format is exportable or not.
+        '''
         return True
 
+    @classproperty
     def isMinifiable(self):
-        '''Tells whether the file format is minifiable or not.'''
+        '''
+        Tells whether the file format is minifiable or not.
+        '''
         return True
