05d4235da400a3b3da0e26c2837bfa91aefc6d99
Paulo Freitas <me@paulofreitas.me>
2016-08-08 15:23:55 -0300

    Improved firebird format module
    - Changed FirebirdFormat properties to use the new @classproperty decorator;
    - Changed FirebirdFormat.isBinary() and FirebirdFormat.isExportable() methods to property methods using the new @classproperty decorator;
    - Improved docstrings formatting;
    - Updated imports.

--- src/lib/dtb/formats/firebird.py
+++ src/lib/dtb/formats/firebird.py
@@ -11,48 +11,69 @@ from __future__ import absolute_import
 
 # Package dependencies
 
+from dtb.core.helpers.decorators import classproperty
 from dtb.formats import Format
 
 # Classes
 
 
 class FirebirdFormat(Format):
-    '''The file format class for Firebird Embedded file format.'''
+    '''
+    The file format class for Firebird Embedded file format.
+    '''
 
-    @property
+    @classproperty
     def name(self):
-        '''The file format name.'''
+        '''
+        The file format name.
+        '''
         return 'firebird'
 
-    @property
+    @classproperty
     def friendlyName(self):
-        '''The file format friendly name.'''
+        '''
+        The file format friendly name.
+        '''
         return 'Firebird Embedded'
 
-    @property
+    @classproperty
     def extension(self):
-        '''The file format extension.'''
+        '''
+        The file format extension.
+        '''
         return '.fdb'
 
-    @property
+    @classproperty
     def type(self):
-        '''The file format type.'''
+        '''
+        The file format type.
+        '''
         return 'Database'
 
-    @property
+    @classproperty
     def mimeType(self):
-        '''The file format media type.'''
+        '''
+        The file format media type.
+        '''
         return None
 
-    @property
+    @classproperty
     def info(self):
-        '''The file format reference info.'''
+        '''
+        The file format reference info.
+        '''
         return 'https://en.wikipedia.org/wiki/Embedded_database#Firebird_Embedded'
 
+    @classproperty
     def isBinary(self):
-        '''Tells whether the file format is binary or not.'''
+        '''
+        Tells whether the file format is binary or not.
+        '''
         return True
 
+    @classproperty
     def isExportable(self):
-        '''Tells whether the file format is exportable or not.'''
+        '''
+        Tells whether the file format is exportable or not.
+        '''
         return True
