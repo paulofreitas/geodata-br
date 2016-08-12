128f6dc77ca29f294799a6f65118823bd7e7ad82
Paulo Freitas <me@paulofreitas.me>
2016-08-12 13:45:53 -0300

    Added new TSV format and exporter modules (closes #17)

---
+++ src/lib/dtb/formats/tsv.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2016 Paulo Freitas
+# MIT License (see LICENSE file)
+'''
+TSV file format package
+'''
+from __future__ import absolute_import
+
+# Imports
+
+# Package dependencies
+
+from dtb.core.helpers.decorators import classproperty
+from dtb.formats import Format
+
+# Classes
+
+
+class TsvFormat(Format):
+    '''
+    The file format class for TSV file format.
+    '''
+
+    @classproperty
+    def name(self):
+        '''
+        The file format name.
+        '''
+        return 'tsv'
+
+    @classproperty
+    def friendlyName(self):
+        '''
+        The file format friendly name.
+        '''
+        return 'TSV'
+
+    @classproperty
+    def extension(self):
+        '''
+        The file format extension.
+        '''
+        return '.tsv'
+
+    @classproperty
+    def type(self):
+        '''
+        The file format type.
+        '''
+        return 'Data Interchange'
+
+    @classproperty
+    def mimeType(self):
+        '''
+        The file format media type.
+        '''
+        return 'text/tab-separated-values'
+
+    @classproperty
+    def info(self):
+        '''
+        The file format reference info.
+        '''
+        return 'https://en.wikipedia.org/wiki/Tab-separated_values'
+
+    @classproperty
+    def isExportable(self):
+        '''
+        Tells whether the file format is exportable or not.
+        '''
+        return True
+
+    @classproperty
+    def isMinifiable(self):
+        '''
+        Tells whether the file format is minifiable or not.
+        '''
+        return True
