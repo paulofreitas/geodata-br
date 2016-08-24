c872ca8ef26ee87bb3fec816d30192d7e4c9e73d
Paulo Freitas <me@paulofreitas.me>
2016-08-24 05:25:09 -0300

    Improved build command module
    - Renamed DatabaseBuilderCommand class to DatasetBuilderCommand;
    - Tweaked DatasetBuilderCommand.configure() method to rename the -b/--bases argument to -d/--datasets;
    - Tweaked DatasetBuilderCommand.description(), DatasetBuilderCommand.usage() and DatasetBuilderCommand.handle() methods for command changes;
    - Changed DatasetBuilderCommand.handle() method to use the new minified datasets directory structure;
    - Improved DatasetBuilderCommand.handle() method to simplify the directories creation.

--- src/lib/dtb/commands/build.py
+++ src/lib/dtb/commands/build.py
@@ -22,9 +22,9 @@ logger = Logger.instance(__name__)
 # Classes
 
 
-class DatabaseBuilderCommand(Command):
+class DatasetBuilderCommand(Command):
     '''
-    The database builder command.
+    The dataset builder command.
     '''
 
     @property
@@ -39,42 +39,42 @@ class DatabaseBuilderCommand(Command):
         '''
         Defines the command description.
         '''
-        return 'Builds a database'
+        return 'Builds a dataset'
 
     @property
     def usage(self):
         '''
         Defines the command usage syntax.
         '''
-        return '%(prog)s -b BASE -r FORMAT -m FORMAT'
+        return '%(prog)s -d DATASET -r FORMAT -m FORMAT'
 
     def configure(self):
         '''
         Defines the command arguments.
         '''
-        bases = DatabaseRepository.listYears()
+        datasets = DatabaseRepository.listYears()
         raw_formats = FormatRepository.listExportableFormatNames()
         minifiable_formats = FormatRepository.listMinifiableFormatNames()
 
-        self.addArgument('-b', '--bases',
-                         metavar='BASE',
+        self.addArgument('-d', '--datasets',
+                         metavar='DATASET',
                          nargs='*',
-                         default=bases,
-                         help=('Database years to build.\n'
+                         default=datasets,
+                         help=('Datasets to build.\n'
                                'Defaults to all available: {}' \
-                                   .format(', '.join(bases))))
+                                   .format(', '.join(datasets))))
         self.addArgument('-r', '--raw',
                          metavar='FORMAT',
                          nargs='*',
                          default=raw_formats,
-                         help=('Raw formats to build the database.\n'
+                         help=('Raw formats to build the dataset.\n'
                                'Defaults to all available: {}' \
                                     .format(', '.join(raw_formats))))
         self.addArgument('-m', '--min',
                          metavar='FORMAT',
                          nargs='*',
                          default=minifiable_formats,
-                         help=('Minifiable formats to build the database.\n'
+                         help=('Minifiable formats to build the dataset.\n'
                                'Defaults to all available: {}' \
                                    .format(', '.join(minifiable_formats))))
 
@@ -83,19 +83,16 @@ class DatabaseBuilderCommand(Command):
         Handles the command.
         '''
         try:
-            for base in args.bases:
-                raw_dir = DATA_DIR / base
-                minified_dir = DATA_DIR / 'minified' / base
+            for dataset in args.datasets:
+                raw_dir = DATA_DIR / dataset
+                minified_dir = DATA_DIR / dataset / 'minified'
 
-                logger.info('> Building %s base...', base)
+                logger.info('> Building %s dataset...', dataset)
 
-                try:
-                    raw_dir.create(parents=True)
-                    minified_dir.create(parents=True)
-                except OSError:
-                    pass
+                raw_dir.create(parents=True)
+                minified_dir.create(parents=True)
 
-                data = DatabaseFactory.fromYear(base).parse()
+                data = DatabaseFactory.fromYear(dataset).parse()
 
                 with raw_dir:
                     for raw_format in args.raw:
