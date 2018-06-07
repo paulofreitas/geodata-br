3758092e7a5b1cbdd796118443d6bc4fa2492b77
Paulo Freitas <me@paulofreitas.me>
2018-06-07 03:52:28 -0300

    Significantly improve dataset entities module
    - Removed unused DatabaseColumn, DatabaseRow and DatabaseData classes;
    - Removed unneeded localization stuff;
    - Added a simplified abstract Entity class implementation (moved from geodatabr.core.types);
    - Added a Relationship alias to ease declaring entity relationships;
    - Added relationships to entity classes;
    - Added new __name__ attributes to entity classes;
    - Removed unused __columns__ attributes from entity classes;
    - Improved docstrings;
    - Updated imports.

--- src/lib/geodatabr/dataset/entities.py
+++ src/lib/geodatabr/dataset/entities.py
@@ -3,37 +3,91 @@
 # Copyright (c) 2013-2018 Paulo Freitas
 # MIT License (see LICENSE file)
 '''
-Database entities module
+Dataset schema module
 
-This module provides the database entities used across the package.
+This module provides the entities used to describe the database.
 '''
 # Imports
 
-# Built-in dependencies
+# Package dependencies
 
-from collections import OrderedDict
+from geodatabr.core.types import OrderedMap
 
 # External dependencies
 
-from sqlalchemy.sql.schema import Column, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column, ForeignKey, MetaData, Table
 from sqlalchemy.types import BigInteger, Integer, SmallInteger, String
 
-# Package dependencies
+# Aliases
 
-from geodatabr.core.i18n import _, Translator
-from geodatabr.core.types import Entity
-
-# Translator setup
-
-Translator.load('databases')
+Relationship = lambda entity, backref, **kwargs: \
+    relationship(entity, back_populates=backref, **kwargs)
 
 # Classes
+
+
+class Entity(declarative_base()):
+    '''
+    Abstract entity class.
+    '''
+    __abstract__ = True
+
+    naming_convention = {
+        'pk': 'pk_%(table_name)s',
+        'fk': 'fk_%(table_name)s_%(column_0_name)s',
+        'ix': 'ix_%(column_0_label)s',
+        'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    }
+
+    metadata = MetaData(naming_convention=naming_convention)
+
+    def serialize(self, flatten=False):
+        '''
+        Serializes the entity to an ordered mapping.
+
+        Args:
+            flatten (bool): Whether it should flatten column names or not
+
+        Returns:
+            geodatabr.core.types.OrderedMap: The entity columns/values pairs
+        '''
+        if not flatten:
+            return Map({column.name: getattr(self, column.name)
+                       for column in self.__table__.columns})
+
+        def _flatten(entity):
+            flattened = Map()
+
+            for column in entity.__table__.columns:
+                if not column.foreign_keys:
+                    flattened[entity.__name__ + '_' + column.name] =\
+                        getattr(entity, column.name)
+
+            return flattened
+
+        flattened = Map()
+
+        for column in reversed(list(self.__table__.columns)):
+            for foreign_key in column.foreign_keys:
+                for relationship in self.__mapper__.relationships:
+                    if relationship.table == foreign_key.column.table:
+                        flattened.update(
+                            _flatten(getattr(self, relationship.key)))
+
+        flattened.update(_flatten(self))
+
+        return flattened
 
 
 class State(Entity):
     '''
     Entity for states.
     '''
+    __name__ = 'state'
+
     __table__ = Table(
         'states',
         Entity.metadata,
@@ -44,20 +98,22 @@ class State(Entity):
         Column('name',
                String(32),
                nullable=False,
-               index=True)
-    )
+               index=True))
 
-    # Columns mapping
-    __columns__ = {
-        'id': 'state_id',
-        'name': 'state_name',
-    }
+    # Relationships
+    mesoregions = Relationship('Mesoregion', 'state')
+    microregions = Relationship('Microregion', 'state')
+    municipalities = Relationship('Municipality', 'state')
+    districts = Relationship('District', 'state')
+    subdistricts = Relationship('Subdistrict', 'state')
 
 
 class Mesoregion(Entity):
     '''
     Entity for mesoregions.
     '''
+    __name__ = 'mesoregion'
+
     __table__ = Table(
         'mesoregions',
         Entity.metadata,
@@ -73,21 +129,22 @@ class Mesoregion(Entity):
         Column('name',
                String(64),
                nullable=False,
-               index=True)
-    )
+               index=True))
 
-    # Columns mapping
-    __columns__ = {
-        'id': 'mesoregion_id',
-        'state_id': 'state_id',
-        'name': 'mesoregion_name',
-    }
+    # Relationships
+    state = Relationship('State', 'mesoregions')
+    microregions = Relationship('Microregion', 'mesoregion')
+    municipalities = Relationship('Municipality', 'mesoregion')
+    districts = Relationship('District', 'mesoregion')
+    subdistricts = Relationship('Subdistrict', 'mesoregion')
 
 
 class Microregion(Entity):
     '''
     Entity for microregions.
     '''
+    __name__ = 'microregion'
+
     __table__ = Table(
         'microregions',
         Entity.metadata,
@@ -108,22 +165,22 @@ class Microregion(Entity):
         Column('name',
                String(64),
                nullable=False,
-               index=True)
-    )
+               index=True))
 
-    # Columns mapping
-    __columns__ = {
-        'id': 'microregion_id',
-        'mesoregion_id': 'mesoregion_id',
-        'state_id': 'state_id',
-        'name': 'microregion_name',
-    }
+    # Relationships
+    state = Relationship('State', 'microregions')
+    mesoregion = Relationship('Mesoregion', 'microregions')
+    municipalities = Relationship('Municipality', 'microregion')
+    districts = Relationship('District', 'microregion')
+    subdistricts = Relationship('Subdistrict', 'microregion')
 
 
 class Municipality(Entity):
     '''
     Entity for municipalities.
     '''
+    __name__ = 'municipality'
+
     __table__ = Table(
         'municipalities',
         Entity.metadata,
@@ -149,23 +206,22 @@ class Municipality(Entity):
         Column('name',
                String(64),
                nullable=False,
-               index=True)
-    )
+               index=True))
 
-    # Columns mapping
-    __columns__ = {
-        'id': 'municipality_id',
-        'microregion_id': 'microregion_id',
-        'mesoregion_id': 'mesoregion_id',
-        'state_id': 'state_id',
-        'name': 'municipality_name',
-    }
+    # Relationships
+    state = Relationship('State', 'municipalities')
+    mesoregion = Relationship('Mesoregion', 'municipalities')
+    microregion = Relationship('Microregion', 'municipalities')
+    districts = Relationship('District', 'municipality')
+    subdistricts = Relationship('Subdistrict', 'municipality')
 
 
 class District(Entity):
     '''
     Entity for districts.
     '''
+    __name__ = 'district'
+
     __table__ = Table(
         'districts',
         Entity.metadata,
@@ -196,24 +252,22 @@ class District(Entity):
         Column('name',
                String(64),
                nullable=False,
-               index=True)
-    )
+               index=True))
 
-    # Columns mapping
-    __columns__ = {
-        'id': 'district_id',
-        'municipality_id': 'municipality_id',
-        'microregion_id': 'microregion_id',
-        'mesoregion_id': 'mesoregion_id',
-        'state_id': 'state_id',
-        'name': 'district_name',
-    }
+    # Relationships
+    state = Relationship('State', 'districts')
+    mesoregion = Relationship('Mesoregion', 'districts')
+    microregion = Relationship('Microregion', 'districts')
+    municipality = Relationship('Municipality', 'districts')
+    subdistricts = Relationship('Subdistrict', 'district')
 
 
 class Subdistrict(Entity):
     '''
     Entity for subdistricts.
     '''
+    __name__ = 'subdistrict'
+
     __table__ = Table(
         'subdistricts',
         Entity.metadata,
@@ -249,267 +303,14 @@ class Subdistrict(Entity):
         Column('name',
                String(64),
                nullable=False,
-               index=True)
-    )
+               index=True))
 
-    # Columns mapping
-    __columns__ = {
-        'id': 'subdistrict_id',
-        'district_id': 'district_id',
-        'municipality_id': 'municipality_id',
-        'microregion_id': 'microregion_id',
-        'mesoregion_id': 'mesoregion_id',
-        'state_id': 'state_id',
-        'name': 'subdistrict_name',
-    }
-
-
-class DatabaseColumn(object):
-    '''
-    Entity class for database columns.
-    '''
-
-    def __init__(self, name, **rules):
-        '''
-        Constructor.
-
-        Arguments:
-            name (str): The column name
-            rules (dict): The column rules
-        '''
-        self.name = name
-        self.rules = rules
-
-
-class DatabaseRow(object):
-    '''
-    Entity class for database rows.
-    '''
-
-    __columns__ = [
-        DatabaseColumn('state_id',
-                       requires='state_name',
-                       cast_type=int),
-        DatabaseColumn('state_name'),
-        DatabaseColumn('mesoregion_id',
-                       requires='mesoregion_name',
-                       min_length=2,
-                       suffix='state_id',
-                       cast_type=int),
-        DatabaseColumn('mesoregion_name'),
-        DatabaseColumn('microregion_id',
-                       requires='microregion_name',
-                       min_length=3,
-                       suffix='state_id',
-                       cast_type=int),
-        DatabaseColumn('microregion_name'),
-        DatabaseColumn('municipality_id',
-                       requires='municipality_name',
-                       min_length=5,
-                       suffix='state_id',
-                       cast_type=int),
-        DatabaseColumn('municipality_name'),
-        DatabaseColumn('district_id',
-                       requires='district_name',
-                       min_length=2,
-                       suffix='municipality_id',
-                       cast_type=int),
-        DatabaseColumn('district_name'),
-        DatabaseColumn('subdistrict_id',
-                       requires='subdistrict_name',
-                       min_length=2,
-                       suffix='district_id',
-                       cast_type=int),
-        DatabaseColumn('subdistrict_name'),
-    ]
-
-    def __init__(self):
-        '''
-        Constructor.
-        '''
-        # Initialize attributes
-        for column in self.__columns__:
-            self.__dict__[column.name] = None
-
-        self._name = None
-
-    def columns(self, localized=False):
-        '''
-        Returns the row column names.
-
-        Arguments:
-            localized (bool): Whether or not it should localize column names
-
-        Returns:
-            list: The row column names
-        '''
-        return [_(column.name) if localized else column.name
-                for column in self.__columns__]
-
-    def values(self):
-        '''
-        Returns the row column values.
-
-        Returns:
-            list: The row column values
-        '''
-        return [self.__dict__[column.name] for column in self.__columns__]
-
-    def normalize(self, forceStr=False):
-        '''
-        Normalizes the row data as needed.
-
-        Arguments:
-            forceStr (bool): Whether or not it should convert columns to string
-
-        Returns:
-            DatabaseRow: The self DatabaseRow instance
-        '''
-        # pylint: disable=unidiomatic-typecheck
-        for column in self.__columns__:
-            value = self.__dict__[column.name]
-
-            # Skip unset values
-            if not value:
-                continue
-
-            # Convert non-string columns
-            if forceStr:
-                value = value.decode() if type(value) == bytes else str(value)
-
-            # Unset/skip unused columns
-            if (('requires' in column.rules
-                    and not self.__dict__[column.rules['requires']])
-                    or ('cast_type' in column.rules
-                        and not column.rules['cast_type'](value))):
-                self.__dict__[column.name] = None
-                continue
-
-            # Reformat columns as needed
-            if 'min_length' in column.rules:
-                if len(value) < column.rules['min_length']:
-                    value = '{:0>{}}'.format(value, column.rules['min_length'])
-
-                if ('suffix' in column.rules
-                        and len(value) == column.rules['min_length']):
-                    value = str(self.__dict__[column.rules['suffix']]) + value
-
-            # Cast columns
-            if 'cast_type' in column.rules:
-                value = column.rules['cast_type'](value)
-
-            # Save normalized value
-            self.__dict__[column.name] = value
-
-        return self
-
-    def serialize(self):
-        '''
-        Returns the serialized row data dictionary.
-
-        Returns:
-            dict: The serialized row data dictionary
-        '''
-        return {_(column.name): self.__dict__[column.name]
-                for column in self.__columns__}
-
-    def __repr__(self):
-        '''
-        Returns the string representation of this object.
-
-        Returns:
-            str: The string representation of this object
-        '''
-        return repr(self.serialize())
-
-
-class DatabaseData(object):
-    '''
-    Entity for database data.
-    '''
-
-    def __init__(self, base, columns, rows, records):
-        '''
-        Constructor.
-
-        Arguments:
-            base (geodatabr.dataset.Database): The database where data belongs
-            columns (list): The parsed columns
-            rows (list): The parsed rows
-            records (geodatabr.core.types.Map): The parsed records
-        '''
-        self._base = base
-        self._columns = columns
-        self._rows = rows
-        self._records = records
-
-    @property
-    def columns(self):
-        '''
-        Returns the database columns.
-
-        Returns:
-            list: The database columns
-        '''
-        return self._columns
-
-    @property
-    def rows(self):
-        '''
-        Returns the database rows.
-
-        Returns:
-            list: The database rows
-        '''
-        return self._rows
-
-    @property
-    def records(self):
-        '''
-        Returns the database records.
-
-        Returns:
-            geodatabr.core.type.Map: The database records
-        '''
-        return self._records
-
-    def normalize(self, strKeys=False, forceStr=False, includeKey=False):
-        '''
-        Converts this database data into an ordered dictionary.
-
-        Arguments:
-            strKeys (bool): Whether or not it should coerce dictionary keys to string
-            forceStr (bool): Whether or not it should coerce dictionary values to string
-            includeKey (bool): Whether or not it should include the primary key
-
-        Returns:
-            collections.OrderedDict: The ordered dictionary
-        '''
-        records = OrderedDict()
-
-        for entity in self._base.entities:
-            if not len(self._records[entity.__table__.name]):
-                continue
-
-            table = _(entity.__table__.name)
-            records[table] = OrderedDict()
-
-            for row in self._records[entity.__table__.name]:
-                record = OrderedDict()
-
-                for column in entity.columns:
-                    value = row[column]
-                    column = _(column)
-                    record[column] = str(value) if forceStr else value
-
-                row_id = str(record['id']) if strKeys else record['id']
-
-                if not includeKey:
-                    del record['id']
-
-                records[table][row_id] = record
-
-        return records
+    # Relationships
+    state = Relationship('State', 'subdistricts')
+    mesoregion = Relationship('Mesoregion', 'subdistricts')
+    microregion = Relationship('Microregion', 'subdistricts')
+    municipality = Relationship('Municipality', 'subdistricts')
+    district = Relationship('District', 'subdistricts')
 
 
 # Collections
