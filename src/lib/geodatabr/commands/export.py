c86bb41b0eff5beb372339783f0b5da176ef6069
Paulo Freitas <me@paulofreitas.me>
2018-06-07 05:57:35 -0300

    Improve export command module
    - Renamed DatabaseExporterCommand class to DatasetExporterCommand;
    - Fixed DatasetExporterCommand.description property text;
    - Changed DatasetExporterCommand.configure() method to include a new -l/--locale configuration option;
    - Changed DatasetExporterCommand.usage property to include the new -l (locale) configuration option;
    - Changed DatasetExporterCommand.handle() method to handle the new -l/--locale configuration option;
    - Fixed DatasetExporterCommand.handle() method to use the ExporterFactory class;
    - Improved docstrings and help/exception messages;
    - Updated imports.

--- src/lib/geodatabr/commands/export.py
+++ src/lib/geodatabr/commands/export.py
@@ -10,8 +10,9 @@ Export command module
 # Package dependencies
 
 from geodatabr.commands import Command
+from geodatabr.core.i18n import Translator
 from geodatabr.core.logging import Logger
-from geodatabr.dataset import DatabaseFactory
+from geodatabr.exporters import ExporterFactory
 from geodatabr.formats import FormatRepository
 
 # Module logging
@@ -21,9 +22,9 @@ logger = Logger.instance(__name__)
 # Classes
 
 
-class DatabaseExporterCommand(Command):
+class DatasetExporterCommand(Command):
     '''
-    The database exporter command.
+    The dataset exporter command.
     '''
 
     @property
@@ -38,41 +39,55 @@ class DatabaseExporterCommand(Command):
         '''
         Defines the command description.
         '''
-        return 'Exports a database'
+        return 'Exports the dataset'
 
     @property
     def usage(self):
         '''
         Defines the command usage syntax.
         '''
-        return '%(prog)s -f FORMAT [-o FILENAME]'
+        return '%(prog)s -f FORMAT [-l LOCALE] [-o FILENAME]'
 
     def configure(self):
         '''
         Defines the command arguments.
         '''
+        locales = Translator.locales()
+
         self.addArgument('-f', '--format',
                          metavar='FORMAT',
                          choices=FormatRepository.listExportableFormatNames(),
-                         help=('Format to export the database.\n'
+                         help=('File format to export the dataset.\n'
                                'Options: %(choices)s'))
+        self.addArgument('-l', '--locale',
+                         metavar='LOCALE',
+                         choices=locales,
+                         default='en',
+                         help=('Locale to export the dataset.\n'
+                               'Options: %(choices)s\n'
+                               'Default: %(default)s'))
         self.addArgument('-o', '--out',
                          dest='filename',
                          nargs='?',
-                         help=('Specify a file to write the export to.\n'
+                         help=('Filename to write the export to.\n'
                                'If none are specified, %(prog)s writes data to '
                                'standard output.'))
 
     def handle(self, args):
         '''
         Handles the command.
+
+        Raises:
+            ExportError: When dataset fails to export
         '''
         if not args.format:
             self._parser.error(
-                'You need to give the database format you want to export.')
+                'You need to give the output format you want to export.')
+
+        Translator.locale = args.locale
 
         try:
-            base = DatabaseFactory.create()
-            base.parse().export(args.format, args.filename)
+            exporter = ExporterFactory.fromFormat(args.format)
+            exporter.exportToFile(args.filename)
         except KeyboardInterrupt:
             logger.info('> Exporting was canceled.')
