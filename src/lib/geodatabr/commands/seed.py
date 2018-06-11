7b0ff468b08690bbb76ef4f6d7e8e248e18fa47a
Paulo Freitas <me@paulofreitas.me>
2018-06-10 23:50:02 -0300

    Add new seed command module

---
+++ src/lib/geodatabr/commands/seed.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2018 Paulo Freitas
+# MIT License (see LICENSE file)
+'''
+Seed command module
+'''
+# Imports
+
+# Package dependencies
+
+from geodatabr.commands import Command
+from geodatabr.core.logging import Logger
+from geodatabr.dataset.base import DatabaseHelper
+from geodatabr.dataset.schema import Entities
+from geodatabr.dataset.seeders import SeederFactory, NothingToSeedError
+
+# Module logging
+
+logger = Logger.instance(__name__)
+
+# Classes
+
+
+class SeedCommand(Command):
+    '''
+    A command class to seed the dataset with records.
+    '''
+
+    @property
+    def name(self):
+        '''
+        Defines the command name.
+        '''
+        return 'seed'
+
+    @property
+    def description(self):
+        '''
+        Defines the command description.
+        '''
+        return 'Seed the dataset with records'
+
+    @property
+    def usage(self):
+        '''
+        Defines the command usage syntax.
+        '''
+        return '%(prog)s'
+
+    def handle(self, args):
+        '''
+        Handles the command.
+        '''
+        try:
+            DatabaseHelper.create()
+
+            for entity in Entities:
+                logger.info('> Seeding table "{}"...' \
+                    .format(entity.__table__.name))
+
+                try:
+                    SeederFactory(entity).run()
+                except NothingToSeedError:
+                    logger.warning('Nothing to seed.')
+        except KeyboardInterrupt:
+            logger.warning('Seeding was canceled.')
