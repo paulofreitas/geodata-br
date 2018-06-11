02403dc57a984d445e7f0f18867bc21a97d52bc3
Paulo Freitas <me@paulofreitas.me>
2018-06-11 15:06:36 -0300

    Clean up core helpers package
    - Removed unneeded package metadata;
    - Removed unused Number class;
    - Updated imports.

--- src/lib/geodatabr/core/helpers/__init__.py
+++ src/lib/geodatabr/core/helpers/__init__.py
@@ -7,17 +7,6 @@ Core helpers package
 
 This package provides utility helper modules.
 '''
-# Package metadata
+# Package imports
 
-__version__ = '1.0-dev'
-__author__ = 'Paulo Freitas <me@paulofreitas.me>'
-__copyright__ = 'Copyright (c) 2013-2018 Paulo Freitas'
-__license__ = 'MIT License'
-
-# Classes
-
-
-class Number(object):
-    @staticmethod
-    def percentDifference(from_value, to_value):
-        return (1 - float(from_value) / float(to_value)) * 100
+from geodatabr import __version__, __author__, __copyright__, __license__
