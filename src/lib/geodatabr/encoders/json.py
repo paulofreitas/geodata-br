b6e39716634765ff4887def15d4eb5eb392ebd0f
Paulo Freitas <me@paulofreitas.me>
2018-06-14 02:27:10 -0300

    Improve json encoder module
    - Changed JsonFormat property methods to use the @property decorator;
    - Renamed JsonEncoder._format property to JsonEncoder.format;
    - Added new JsonEncoder.serializer property;
    - Added new JsonEncoder.options property method;
    - Changed JsonEncoder.encode() method to require a new "data" argument;
    - Improved JsonEncoder.encode() method to allow changing default encoding options;
    - Improved JsonEncoder.encode() method to return a FileStream instance;
    - Improved JsonEncoder.encode() method to raise an EncodeError exception on exceptions;
    - Added useful type-hinting to all methods;
    - Improved and fixed docstrings;
    - Cleaned unused imports.

--- src/lib/geodatabr/encoders/json.py
+++ src/lib/geodatabr/encoders/json.py
@@ -2,98 +2,92 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2013-2018 Paulo Freitas
 # MIT License (see LICENSE file)
-'''
-JSON file encoder module
-'''
+'''JSON encoder module.'''
 # Imports
 
 # Built-in dependencies
 
-import io
 import json
 
 # Package dependencies
 
-from geodatabr.core.helpers.decorators import classproperty
+from geodatabr.core.types import FileStream
 from geodatabr.dataset.serializers import Serializer
-from geodatabr.encoders import Encoder, EncoderFormat
+from geodatabr.encoders import Encoder, EncoderFormat, EncodeError
 
 # Classes
 
 
 class JsonFormat(EncoderFormat):
-    '''
-    The file format class for JSON file format.
-    '''
+    '''Encoder format class for JSON file format.'''
 
-    @classproperty
-    def name(self):
-        '''
-        The file format name.
-        '''
+    @property
+    def name(self) -> str:
+        '''Gets the encoder format name.'''
         return 'json'
 
-    @classproperty
-    def friendlyName(self):
-        '''
-        The file format friendly name.
-        '''
+    @property
+    def friendlyName(self) -> str:
+        '''Gets the encoder format friendly name.'''
         return 'JSON'
 
-    @classproperty
-    def extension(self):
-        '''
-        The file format extension.
-        '''
+    @property
+    def extension(self) -> str:
+        '''Gets the encoder format extension.'''
         return '.json'
 
-    @classproperty
-    def type(self):
-        '''
-        The file format type.
-        '''
+    @property
+    def type(self) -> str:
+        '''Gets the encoder format type.'''
         return 'Data Interchange'
 
-    @classproperty
-    def mimeType(self):
-        '''
-        The file format media type.
-        '''
+    @property
+    def mimeType(self) -> str:
+        '''Gets the encoder format media type.'''
         return 'application/json'
 
-    @classproperty
-    def info(self):
-        '''
-        The file format reference info.
-        '''
+    @property
+    def info(self) -> str:
+        '''Gets the encoder format reference info.'''
         return 'https://en.wikipedia.org/wiki/JSON'
 
 
 class JsonEncoder(Encoder):
     '''
     JSON encoder class.
+
+    Attributes:
+        format (geodatabr.encoders.json.JsonFormat): The encoder format class
+        serializer (geodatabr.dataset.serializers.Serializer):
+            The encoder serialization class
     '''
 
-    # Encoder format
-    _format = JsonFormat
+    format = JsonFormat
+    serializer = Serializer
 
-    def encode(self, **options):
+    @property
+    def options(self) -> dict:
+        '''Gets the default encoding options.'''
+        return dict(indent=2,
+                    separators=(',', ': '),
+                    ensure_ascii=False)
+
+    def encode(self, data: dict, **options) -> FileStream:
         '''
         Encodes the data into a JSON file-like stream.
 
-        Arguments:
-            options (dict): The encoding options
+        Args:
+            data: The data to encode
+            **options: The encoding options
 
         Returns:
-            io.StringIO: A JSON file-like stream
+            A JSON file-like stream
 
         Raises:
-            geodatabr.encoders.EncodeError: When data fails to encode
+            geodatabr.encoders.EncodeError: If data fails to encode
         '''
-        data = Serializer().serialize()
-        json_data = json.dumps(data,
-                               indent=2,
-                               separators=(',', ': '),
-                               ensure_ascii=False)
-
-        return io.StringIO(json_data)
+        try:
+            return FileStream(json.dumps(data,
+                                         **dict(self.options, **options)))
+        except Exception:
+            raise EncodeError
