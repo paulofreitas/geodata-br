f53eee23eea6d23761ae094beae3b86be6abdbb1
Paulo Freitas <me@paulofreitas.me>
2018-06-16 09:48:41 -0300

    Add support for Microsoft Excel Spreadsheet format (closes #9)
    - Renamed XlsFormat class to MicrosoftExcelFormat;
    - Added new MicrosoftExcelEncoder class;
    - Updated imports.

--- src/lib/geodatabr/encoders/xls.py
+++ src/lib/geodatabr/encoders/xls.py
@@ -5,14 +5,20 @@
 """Microsoft Excel Spreadsheet file encoder module."""
 # Imports
 
+# External dependencies
+
+from xlwt import Workbook
+
 # Package dependencies
 
-from geodatabr.encoders import EncoderFormat
+from geodatabr.core.types import BinaryFileStream
+from geodatabr.dataset.serializers import Serializer
+from geodatabr.encoders import Encoder, EncoderFormat, EncodeError
 
 # Classes
 
 
-class XlsFormat(EncoderFormat):
+class MicrosoftExcelFormat(EncoderFormat):
     """Encoder format class for Microsoft Excel Spreadsheet file format."""
 
     @property
@@ -49,3 +55,59 @@ class XlsFormat(EncoderFormat):
     def isBinary(self) -> bool:
         """Tells whether the file format is binary or not."""
         return True
+
+
+class MicrosoftExcelEncoder(Encoder):
+    """
+    Microsoft Excelt Spreadsheet encoder class.
+
+    Attributes:
+        format (geodatabr.encoders.xls.MicrosoftExcelFormat):
+            The encoder format class
+        serializer (geodatabr.dataset.serializers.Serializer):
+            The encoder serialization class
+    """
+
+    format = MicrosoftExcelFormat
+    serializer = Serializer
+
+    def encode(self, data: dict, **options) -> BinaryFileStream:
+        """
+        Encodes the data into a Microsoft Excel Spreadsheet file-like stream.
+
+        Arguments:
+            data: The data to encode
+            **options: The encoding options
+
+        Returns:
+            A Microsoft Excel Spreadsheet file-like stream
+
+        Raises:
+            geodatabr.encoders.EncodeError: If data fails to encode
+        """
+        try:
+            xls_file = BinaryFileStream()
+            workbook = Workbook(encoding='utf-8')
+
+            for entity, records in data.items():
+                sheet = workbook.add_sheet(entity)
+                row_id = 0
+
+                for record in records:
+                    if row_id == 0:
+                        for col_id, (column, _) in enumerate(record.items()):
+                            sheet.write(row_id, col_id, column)
+
+                        row_id += 1
+
+                    for col_id, (_, value) in enumerate(record.items()):
+                        sheet.write(row_id, col_id, value)
+
+                    row_id += 1
+
+            workbook.save(xls_file)
+            xls_file.seek(0)
+
+            return xls_file
+        except Exception:
+            raise EncodeError
