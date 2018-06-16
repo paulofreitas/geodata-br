d85c8bfb403cf1181568bd334eea82500352caaf
Paulo Freitas <me@paulofreitas.me>
2018-06-15 23:01:57 -0300

    Clean up core package
    - Imported package metadata.

--- src/lib/geodatabr/core/__init__.py
+++ src/lib/geodatabr/core/__init__.py
@@ -9,14 +9,8 @@ This package provides all the reusable stuff used in other packages.
 """
 # Package imports
 
+from geodatabr import __version__, __author__, __copyright__, __license__
 from geodatabr.core.helpers.filesystem import Directory, Path
-
-# Package metadata
-
-__version__ = '1.0-dev'
-__author__ = 'Paulo Freitas <me@paulofreitas.me>'
-__copyright__ = 'Copyright (c) 2013-2018 Paulo Freitas'
-__license__ = 'MIT License'
 
 # Constants
 
