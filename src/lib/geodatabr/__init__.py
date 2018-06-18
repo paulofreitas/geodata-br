9a710d12323c1f08530ee25e8424ec476038952b
Paulo Freitas <me@paulofreitas.me>
2018-06-18 13:01:24 -0300

    Fix package metadata
    - Load __license_text__ from package resource when available;
    - Fixed VERSION constant.

--- src/lib/geodatabr/__init__.py
+++ src/lib/geodatabr/__init__.py
@@ -3,16 +3,21 @@
 # Copyright (c) 2013-2018 Paulo Freitas
 # MIT License (see LICENSE file)
 """Brazilian geographic data exporter package."""
-# Package metadata
+# Package imports
 
-VERSION = (18, 6, 'dev')  # CalVer YY.MM[.ID] (see https://calver.org/)
+from pkg_resources import resource_string
+
+# Constants
+
+VERSION = (18, 6, 'dev0')  # CalVer YY.MM[.ID] (see https://calver.org/)
+
+# Package metadata
 
 __package_name__ = 'geodatabr'
 __description__ = 'Brazilian geographic data exporter'
 __version__ = '.'.join(map(str, VERSION))
 __copyright__ = 'Copyright (c) 2013-2018 Paulo Freitas'
 __license__ = 'MIT'
-__license_text__ = open('../../LICENSE').read()
 __url__ = 'https://github.com/paulofreitas/geodata-br'
 __author_name__ = 'Paulo Freitas'
 __author_email__ = 'me@paulofreitas.me'
@@ -21,3 +26,8 @@ __prolog__ = '{0}, version {1}\n{2}' \
              .format(__package_name__, __version__, __copyright__)
 __epilog__ = 'Report bugs and feature requests to <{0}>.' \
              .format(__url__ + '/issues')
+
+try:
+    __license_text__ = resource_string(__name__, 'LICENSE').decode()
+except RuntimeError:
+    __license_text__ = ''
