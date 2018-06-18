fe5532af18aa1975cbc98a7a6a493771d37ebde2
Paulo Freitas <me@paulofreitas.me>
2018-06-18 12:15:08 -0300

    Improve setup script
    - Fixed package metadata reading;
    - Added package data symlinking;
    - Changed setup to copy project license and data files;
    - Updated package dependencies;
    - Updated imports.

--- src/setup.py
+++ src/setup.py
@@ -7,53 +7,66 @@
 
 # Built-in dependencies
 
-import sys
-
+from os import symlink, unlink
+from os.path import abspath
 from setuptools import setup, find_packages
+from sys import version_info
 
 # Compatibility check
 
-if sys.version_info[:2] < (3, 4):
+if version_info[:2] < (3, 4):
     raise RuntimeError('Python version >= 3.4 required')
 
 # Routines
 
-with open('lib/geodatabr/__init__.py') as package:
-    __metadata__ = exec(package.read())  # pylint: disable=exec-used
+try:
+    __metadata__ = {}
 
-setup(
-    # Package metadata
-    name=__metadata__['__package_name__'],
-    version=__metadata__['__version__'],
-    description=__metadata__['__description__'],
-    license=__metadata__['__license__'],
-    url=__metadata__['__url__'],
-    author=__metadata__['__author_name__'],
-    author_email=__metadata__['__author_email__'],
+    with open('lib/geodatabr/__init__.py') as package:
+        exec(package.read(), __metadata__)  # pylint: disable=exec-used
 
-    # Package distribution
-    packages=find_packages('lib'),
-    package_dir={'': 'lib'},
-    entry_points={
-        'console_scripts': [
-            'geodatabr = geodatabr.__main__:main'
+    symlink(abspath('data'), abspath('lib/geodatabr/data'))
+
+    setup(
+        # Package metadata
+        name=__metadata__['__package_name__'],
+        version=__metadata__['__version__'],
+        description=__metadata__['__description__'],
+        license=__metadata__['__license__'],
+        url=__metadata__['__url__'],
+        author=__metadata__['__author_name__'],
+        author_email=__metadata__['__author_email__'],
+
+        # Package distribution
+        packages=find_packages('lib'),
+        package_dir={'': 'lib'},
+        package_data={'': ['data/stubs/*', 'data/translations/*']},
+        data_files=[('geodatabr', ['LICENSE'])],
+        include_package_data=True,
+        entry_points={
+            'console_scripts': [
+                'geodatabr = geodatabr.__main__:main'
+            ],
+        },
+
+        # Package dependencies
+        python_requires='>=3.4',
+        install_requires=[
+            # geodatabr package
+            'requests',
+            'ratelimit',
+            # geodatabr.dataset package
+            'sqlalchemy',
+            # geodatabr.encoders package
+            'fdb',
+            'lxml',
+            'msgpack',
+            'py-ubjson',
+            'pyexcel-ods',
+            'pyexcel-xls',
+            'pyexcel-xlsx',
+            'pyyaml',
         ],
-    },
-
-    # Package dependencies
-    python_requires='>=3.4',
-    install_requires=[
-        # geodatabr package
-        'requests',
-        'ratelimit',
-        # geodatabr.encoders package
-        'fdb',
-        'lxml',
-        'msgpack',
-        'py-ubjson',
-        'pyexcel-ods',
-        'pyexcel-xls',
-        'pyyaml',
-        'sqlalchemy',
-    ]
-)
+    )
+finally:
+    unlink(abspath('lib/geodatabr/data'))
