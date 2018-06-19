4ff7f6db4c614f94b4a0ad89341feed9ffb9c09e
Paulo Freitas <me@paulofreitas.me>
2018-06-19 00:13:30 -0300

    Clean up core package
    - Removed unused hard-coded PKG_DIR, LIB_DIR, SRC_DIR, BASE_DIR and DATA_DIR constants;
    - Cleaned unused imports.

--- src/lib/geodatabr/core/__init__.py
+++ src/lib/geodatabr/core/__init__.py
@@ -10,15 +10,6 @@ This package provides all the reusable stuff used in other packages.
 # Package imports
 
 from geodatabr import __author__, __copyright__, __license__, __version__
-from geodatabr.core.helpers.filesystem import Directory, Path
-
-# Constants
-
-PKG_DIR = Directory(Path(__file__).parents[1])
-LIB_DIR = Directory(PKG_DIR.parent)
-SRC_DIR = Directory(LIB_DIR.parent)
-BASE_DIR = Directory(SRC_DIR.parent)
-DATA_DIR = Directory(Path().cwd())
 
 # Package exports
 
@@ -27,9 +18,4 @@ __all__ = [
     '__copyright__',
     '__license__',
     '__version__',
-    'BASE_DIR',
-    'DATA_DIR',
-    'LIB_DIR',
-    'PKG_DIR',
-    'SRC_DIR',
 ]
