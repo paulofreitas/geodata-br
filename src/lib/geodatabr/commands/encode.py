936e132f30e6442e08e5579ab740b6022bf1a512
Paulo Freitas <me@paulofreitas.me>
2018-06-29 09:11:46 -0300

    Improve encode command module
    - Simplified module importing;
    - Changed EncodeCommand.configure() method to remove the support for custom filenames through the -o/--out argument option;
    - Changed EncodeCommand.configure() method to add support for selecting dataset tables through a new -t/--tables argument option;
    - Changed EncodeCommand.usage() method for the new usage syntax;
    - Changed EncodeCommand.handle() method to remove the stuff for custom filenames support;
    - Changed EncodeCommand.handle() method to use the simplified Serializer class;
    - Improved EncodeCommand.handle() method to write files within the data directory;
    - Improved EncodeCommand.handle() method to serialize only the requested dataset tables;
    - Improved EncodeCommand.handle() method to serialize flat file formats to one file per dataset table;
    - Improved EncodeCommand.handle() method to exit with proper exit codes in case of errors/interruptions.

--- src/lib/geodatabr/commands/encode.py
+++ src/lib/geodatabr/commands/encode.py
@@ -7,20 +7,18 @@
 
 # Built-in dependencies
 
-from argparse import Namespace
+import argparse
 
 # Package dependencies
 
-from geodatabr.core.commands import Command
-from geodatabr.core.encoders import EncoderFactory, EncoderFormatRepository, \
-    EncodeError
-from geodatabr.core.i18n import Translator
-from geodatabr.core.logging import logger
+from geodatabr.core import commands, encoders, i18n, logging
+from geodatabr.core.utils import io
+from geodatabr.dataset import schema, serializers
 
 # Classes
 
 
-class EncodeCommand(Command):
+class EncodeCommand(commands.Command):
     """A command class to encode the dataset."""
 
     @property
@@ -36,51 +34,68 @@ class EncodeCommand(Command):
     @property
     def usage(self) -> str:
         """Gets the command usage syntax."""
-        return '%(prog)s -f FORMAT [-l LOCALE] [-o FILENAME]'
+        return '%(prog)s -f FORMAT [-l LOCALE] [-t TABLES]'
 
     def configure(self):
         """Defines the command arguments."""
         self.addArgument('-f', '--format',
                          metavar='FORMAT',
-                         choices=EncoderFormatRepository.listNames(),
+                         choices=encoders.EncoderFormatRepository.listNames(),
                          help=('File format to encode the dataset.\n'
                                'Options: %(choices)s'))
         self.addArgument('-l', '--locale',
                          metavar='LOCALE',
-                         choices=Translator.locales(),
+                         choices=i18n.Translator.locales(),
                          default='en',
                          help=('Locale to encode the dataset.\n'
                                'Options: %(choices)s\n'
                                'Default: %(default)s'))
-        self.addArgument('-o', '--out',
-                         dest='filename',
-                         nargs='?',
-                         default='-',
-                         help=('Filename to save the dataset.\n'
-                               'If none are specified, data is written to '
-                               'standard output.'))
+        self.addArgument('-t', '--tables',
+                         metavar='TABLES',
+                         nargs='*',
+                         choices=schema.TABLES,
+                         default=schema.TABLES,
+                         help=('Dataset tables to encode.\n'
+                               'Options: %(choices)s\n'
+                               'Default: All tables'))
 
-    def handle(self, args: Namespace):
+    def handle(self, args: argparse.Namespace):
         """Handles the command."""
         if not args.format:
             self._parser.error(
                 'You need to give the output format you want to encode.')
 
-        Translator.locale = args.locale
+        i18n.Translator.locale = args.locale
+        logger = logging.logger()
 
         try:
-            encoder = EncoderFactory.fromFormat(args.format)
+            encoder = encoders.EncoderFactory.fromFormat(args.format)
+            serializer = serializers.Serializer(**encoder.serializationOptions)
+            entity_map = dict(zip(schema.TABLES, schema.ENTITIES))
 
-            if args.filename == '-' and encoder.format.isBinary:
-                self._parser.error(
-                    "Binary formats can't be written to standard output")
+            logger.info('Encoding dataset to %s format...',
+                        encoder.format.friendlyName)
 
-            logger().info('Encoding dataset to %s format...',
-                          encoder.format().friendlyName)
+            dataset_dir = io.Directory(io.Path.DATA_DIR / args.locale)
+            dataset_dir.create(parents=True)
 
-            serializer = encoder.serializer(**encoder.serializationOptions)
-            encoder.encodeToFile(serializer.serialize(), args.filename)
-        except EncodeError:
-            logger().error('Failed to encode dataset.')
+            with dataset_dir:
+                if encoder.format.isFlatFile:
+                    for table in args.tables:
+                        entity = (entity_map.get(table),)
+                        encoder.encodeToFile(
+                            serializer.serialize(entity).get(table),
+                            '{dataset_name}-{table_name}'.format(
+                                dataset_name=i18n._('dataset_name'),
+                                table_name=table) + encoder.format.extension)
+
+                    self._parser.exit(0)
+
+                encoder.encodeToFile(
+                    serializer.serialize(tuple(entity_map.get(table)
+                                               for table in args.tables)),
+                    i18n._('dataset_name') + encoder.format.extension)
+        except encoders.EncodeError:
+            self._parser.error('Failed to encode dataset.')
         except KeyboardInterrupt:
-            logger().info('Encoding was canceled.')
+            self._parser.terminate('Encoding was canceled.')
