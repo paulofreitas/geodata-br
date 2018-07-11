058c6a303d4f881e4a98e89b093fda818aa62e5d
Paulo Freitas <me@paulofreitas.me>
2018-07-11 12:07:41 -0300

    Clean up datasets schema module
    - Removed unused Entity class (moved to geodatabr.core.datasets module);
    - Removed unused module imports;
    - Tweaked module importing;
    - Tweaked docstrings.

--- src/lib/geodatabr/dataset/schema.py
+++ src/lib/geodatabr/dataset/schema.py
@@ -3,75 +3,38 @@
 # Copyright (c) 2013-2018 Paulo Freitas
 # MIT License (see LICENSE file)
 """
-Dataset schema module.
+Datasets schema module.
 
-This module provides the entities used to describe the database.
+This module provides the entities used to describe the datasets.
 """
 # Imports
 
 # External dependencies
 
-from sqlalchemy import orm, types as sql_types
-from sqlalchemy.ext import declarative
+from sqlalchemy import orm, types
 from sqlalchemy.sql import schema
 
 # Package dependencies
 
-from geodatabr.core import types
+from geodatabr.core import datasets
 
 # Classes
 
 
-class Entity(declarative.declarative_base()):
-    """
-    Abstract entity class.
-
-    Attributes:
-        naming_convention (dict): The constraint naming conventions
-        metadata (sqlalchemy.sql.schema.MetaData): The entity metadata
-    """
-
-    __abstract__ = True
-
-    naming_convention = {
-        'pk': 'pk_%(table_name)s',
-        'fk': 'fk_%(table_name)s_%(column_0_name)s',
-        'uq': 'uq_%(table_name)s_%(column_0_name)s',
-        'ix': 'ix_%(column_0_label)s',
-    }
-
-    metadata = schema.MetaData(naming_convention=naming_convention)
-
-    def serialize(self, columns: list = None) -> types.OrderedMap:
-        """
-        Serializes the entity to an ordered mapping.
-
-        Args:
-            columns: An optional list of column names to serialize
-
-        Returns:
-            The entity columns/values pairs
-        """
-        columns = columns or [column.name for column in self.__table__.columns]
-
-        return types.OrderedMap((str(column), getattr(self, column))
-                                for column in columns)
-
-
-class State(Entity):
+class State(datasets.Entity):
     """Entity class for states."""
 
     _name = 'state'
 
     __table__ = schema.Table(
         'states',
-        Entity.metadata,
+        datasets.Entity.metadata,
         schema.Column('id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       nullable=False,
                       primary_key=True),
         schema.Column('name',
-                      sql_types.String(32),
+                      types.String(32),
                       nullable=False,
                       index=True))
 
@@ -83,25 +46,25 @@ class State(Entity):
     subdistricts = orm.relationship('Subdistrict', back_populates='state')
 
 
-class Mesoregion(Entity):
+class Mesoregion(datasets.Entity):
     """Entity class for mesoregions."""
 
     _name = 'mesoregion'
 
     __table__ = schema.Table(
         'mesoregions',
-        Entity.metadata,
+        datasets.Entity.metadata,
         schema.Column('id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       nullable=False,
                       primary_key=True),
         schema.Column('state_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('states.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('name',
-                      sql_types.String(64),
+                      types.String(64),
                       nullable=False,
                       index=True))
 
@@ -116,30 +79,30 @@ class Mesoregion(Entity):
         orm.relationship('Subdistrict', back_populates='mesoregion')
 
 
-class Microregion(Entity):
+class Microregion(datasets.Entity):
     """Entity class for microregions."""
 
     _name = 'microregion'
 
     __table__ = schema.Table(
         'microregions',
-        Entity.metadata,
+        datasets.Entity.metadata,
         schema.Column('id',
-                      sql_types.Integer,
+                      types.Integer,
                       nullable=False,
                       primary_key=True),
         schema.Column('mesoregion_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('mesoregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('state_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('states.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('name',
-                      sql_types.String(64),
+                      types.String(64),
                       nullable=False,
                       index=True))
 
@@ -153,35 +116,35 @@ class Microregion(Entity):
         orm.relationship('Subdistrict', back_populates='microregion')
 
 
-class Municipality(Entity):
+class Municipality(datasets.Entity):
     """Entity class for municipalities."""
 
     _name = 'municipality'
 
     __table__ = schema.Table(
         'municipalities',
-        Entity.metadata,
+        datasets.Entity.metadata,
         schema.Column('id',
-                      sql_types.Integer,
+                      types.Integer,
                       nullable=False,
                       primary_key=True),
         schema.Column('microregion_id',
-                      sql_types.Integer,
+                      types.Integer,
                       schema.ForeignKey('microregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('mesoregion_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('mesoregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('state_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('states.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('name',
-                      sql_types.String(64),
+                      types.String(64),
                       nullable=False,
                       index=True))
 
@@ -196,40 +159,40 @@ class Municipality(Entity):
         orm.relationship('Subdistrict', back_populates='municipality')
 
 
-class District(Entity):
+class District(datasets.Entity):
     """Entity class for districts."""
 
     _name = 'district'
 
     __table__ = schema.Table(
         'districts',
-        Entity.metadata,
+        datasets.Entity.metadata,
         schema.Column('id',
-                      sql_types.Integer,
+                      types.Integer,
                       nullable=False,
                       primary_key=True),
         schema.Column('municipality_id',
-                      sql_types.Integer,
+                      types.Integer,
                       schema.ForeignKey('municipalities.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('microregion_id',
-                      sql_types.Integer,
+                      types.Integer,
                       schema.ForeignKey('microregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('mesoregion_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('mesoregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('state_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('states.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('name',
-                      sql_types.String(64),
+                      types.String(64),
                       nullable=False,
                       index=True))
 
@@ -242,45 +205,45 @@ class District(Entity):
     subdistricts = orm.relationship('Subdistrict', back_populates='district')
 
 
-class Subdistrict(Entity):
+class Subdistrict(datasets.Entity):
     """Entity class for subdistricts."""
 
     _name = 'subdistrict'
 
     __table__ = schema.Table(
         'subdistricts',
-        Entity.metadata,
+        datasets.Entity.metadata,
         schema.Column('id',
-                      sql_types.BigInteger,
+                      types.BigInteger,
                       nullable=False,
                       primary_key=True),
         schema.Column('district_id',
-                      sql_types.Integer,
+                      types.Integer,
                       schema.ForeignKey('districts.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('municipality_id',
-                      sql_types.Integer,
+                      types.Integer,
                       schema.ForeignKey('municipalities.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('microregion_id',
-                      sql_types.Integer,
+                      types.Integer,
                       schema.ForeignKey('microregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('mesoregion_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('mesoregions.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('state_id',
-                      sql_types.SmallInteger,
+                      types.SmallInteger,
                       schema.ForeignKey('states.id', use_alter=True),
                       nullable=False,
                       index=True),
         schema.Column('name',
-                      sql_types.String(64),
+                      types.String(64),
                       nullable=False,
                       index=True))
 
